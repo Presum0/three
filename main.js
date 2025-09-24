fd6d5c8bcaa1d198bc14e0d2c417b7e221d426b2
Presum0 <preethamnelaturi@gmail.com>
2025-09-24 10:01:46 +0530

    rotation

--- main.js
+++ main.js
@@ -12,9 +12,11 @@ const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
-camera.position.z = 5;
+camera.position.z = 2;
 
 function animate() {
+    cube.rotation.x+=0.01;
+    cube.rotation.y+=0.01;
     renderer.render( scene, camera );
 }
 renderer.setAnimationLoop( animate );
