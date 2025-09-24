c69da23079411cc1d333bbc799462d8cf7976d9b
Presum0 <preethamnelaturi@gmail.com>
2025-09-24 18:45:21 +0530

    lines

--- main.js
+++ main.js
@@ -1,25 +1,12 @@
 import * as THREE from 'three';
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
+const camera = new THREE.PerspectiveCamera(45, window.innerHeight/window.innerWidth, 1, 500);
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-renderer.setAnimationLoop( animate );
-document.body.appendChild( renderer.domElement );
+renderer.setSize(window.innerHeight, window.innerWidth);
+document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+camera.position.set(0,0,100);
+camera.lookAt(0,0,0);
 
-camera.position.z = 5;
-
-function animate() {
-
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-
-  renderer.render( scene, camera );
-
-}
+const material = new THREE.LineBasicMaterial({color: 0x0000ff});
