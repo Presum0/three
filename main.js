314638ea506959b9525681326b794cb4b8c5a7c2
Presum0 <preethamnelaturi@gmail.com>
2025-09-25 19:11:19 +0530

    cube'

---
+++ main.js
@@ -0,0 +1,23 @@
+import * as THREE from 'three';
+
+function main(){
+    const canvas = document.querySelector('#c');
+    const renderer = new THREE.WebGLRenderer({antialias: true, canvas});
+    const fov = 75; 
+    const near = 0.1;
+    const far = 5;
+    const aspect = 2;
+    const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+    camera.position.z = 2;
+    const scene = new THREE.Scene();
+    const boxWidth = 1;
+    const boxheight = 1;
+    const boxDepth = 1;
+    const geometry = new THREE.BoxGeometry(boxWidth, boxheight, boxDepth);
+    const material = new THREE.MeshBasicMaterial({color: 0x44aa88});
+    const cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+    renderer.render(scene,camera);
+}
+
+      main();
