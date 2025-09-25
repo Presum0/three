d1502ac0381d1cc6e0b5ada71f5a69c1ce1c98e6
Presum0 <preethamnelaturi@gmail.com>
2025-09-25 18:06:37 +0530

    arrow

--- main.js
+++ main.js
@@ -10,3 +10,13 @@ camera.position.set(0,0,100);
 camera.lookAt(0,0,0);
 
 const material = new THREE.LineBasicMaterial({color: 0x0000ff});
+const points = [];
+points.push( new THREE.Vector3( - 10, 0, 0 ) );
+points.push( new THREE.Vector3( 0, 10, 0 ) );
+points.push( new THREE.Vector3( 10, 0, 0 ) );
+
+const geometry = new THREE.BufferGeometry().setFromPoints( points );
+
+const line = new THREE.Line(geometry, material);
+scene.add(line);
+renderer.render(scene,camera);
