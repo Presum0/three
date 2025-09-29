da50c30645049b83ba3e01b5a0549d9e63505438
Presum0 <preethamnelaturi@gmail.com>
2025-09-29 17:56:08 +0530

    three cubes

--- main.js
+++ main.js
@@ -15,20 +15,34 @@ function main(){
     const boxheight = 1;
     const boxDepth = 1;
     const geometry = new THREE.BoxGeometry(boxWidth, boxheight, boxDepth);
-    const material = new THREE.MeshBasicMaterial({color: 0x44aa88});
-    const cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
-    renderer.render(scene,camera);
     const color = 0xFFFFFF;
     const intensity = 3;
     const light = new THREE.DirectionalLight(color, intensity);
     light.position.set(-1,2,4);
     scene.add(light);
 
+    function makeInstance(geometry,color,x){
+        const material = new THREE.MeshPhongMaterial({color});
+        const cube = new THREE.Mesh(geometry, material);
+        scene.add(cube);
+        cube.position.x = x;
+        return cube;
+    }
+    
+    const cubes = [
+        makeInstance(geometry, 0x44aa88,  0),
+        makeInstance(geometry, 0x8844aa, -2),
+        makeInstance(geometry, 0xaa8844,  2),
+    ]
+
     function render(time){
         time*=0.001;
-        cube.rotation.x = time;
-        cube.rotation.y = time;
+        cubes.forEach((cube, ndx) => {
+            const speed = 1 + ndx * .1;
+            const rot = time * speed;
+            cube.rotation.x = rot;
+            cube.rotation.y = rot;
+          });
         renderer.render(scene,camera);
         requestAnimationFrame(render);
     }
